e313c77ab24035f2049f473ced5430b4d9139116
Felipa Mendez <felipalouise@gmail.com>
2022-01-28 14:12:30 -0600

    commit tree 5d37b56ae61edaba01edf7e1cac91c8ca19fcffe

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your Circle class here
+
+class Circle {
+    radius;
+
+    constructor(radius) {
+        this.radius = radius
+    }
+
+    set radius(radius) {
+        this.radius = radius
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    set diameter(newDiameter) {
+        this.radius = newDiameter/2
+    }
+
+    get circumference() {
+        return Math.PI * this.radius * 2
+    }
+
+    set circumference(newCircumference) {
+        this.radius = (newCircumference/Math.PI)/2
+    }
+
+    get area() {
+        return Math.PI * this.radius**2
+    }
+    
+    set area(newArea) {
+        this.radius = Math.sqrt(newArea/Math.PI)
+    }
+
+}
+const circle = new Circle(2)
+circle.circumference()
